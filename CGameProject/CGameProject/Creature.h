0824fe9e23d86ae07443079b58ba1fdc8cf0e3bf
Andrey Chetporov <123196902+andreychetporov@users.noreply.github.com>
2025-06-09 23:38:28 +0300

    Update Creature.h

--- CGameProject/CGameProject/Creature.h
+++ CGameProject/CGameProject/Creature.h
@@ -1,15 +1,27 @@
-#pragma once
-#ifndef Creature
-#define Creature
+#ifndef CREATURE
+#define CREATURE
 
 #include <vector>
 #include <memory>
 
+#include "Item.h"
+
 class Creature
 {
-protected:
+private:
+	std::vector<std::shared_ptr<Item>> inventory;
 	int hp;
 	int armor;
+	int money;
+public:
+	std::vector<std::shared_ptr<Item>>& GetInventory();
+	int GetHp() const;
+	int GetArmor() const;
+	int GetMoney() const;
+
+	void SetHp(int new_hp);
+	void SetArmor(int new_armor);
+	void SetMoney(int new_money);
 };
 
-#endif // !Creature 
+#endif // !CREATURE
