7f3feee1e2a64eee50cd8d4fe16e37bf2d71ef22
Andrey Chetporov <achetporov@gmail.com>
2025-06-18 21:40:18 +0300

    update Creature class (json read)
    Добавлен конструктор, который читает все данные из json, добавлены поля: name, strength, dialogues.

--- CGameProject/CGameProject/Creature.cpp
+++ CGameProject/CGameProject/Creature.cpp
@@ -1,5 +1,64 @@
 #include "Creature.h"
 
+
+Creature::Creature(const std::string& json_filename) {
+    std::ifstream file(json_filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Не удалось открыть файл JSON: " + json_filename);
+    }
+
+    json j;
+    file >> j;
+
+    if (j.contains("Name")) {
+        name = j["Name"].get<std::string>();
+    }
+
+    if (j.contains("Strength")) {
+        strength = std::stoi(j["Strength"].get<std::string>());
+    }
+
+    if (j.contains("Hp")) {
+        hp = std::stoi(j["Hp"].get<std::string>());
+    }
+    else {
+        hp = 0;
+    }
+
+    if (j.contains("Armor")) {
+        armor = std::stoi(j["Armor"].get<std::string>());
+    }
+    else {
+        armor = 0;
+    }
+
+    if (j.contains("Money")) {
+        money = std::stoi(j["Money"].get<std::string>());
+    }
+    else {
+        money = 0;
+    }
+
+    if (j.contains("Dialogues")) {
+        dialogues.clear();
+        for (auto& [key, arr] : j["Dialogues"].items()) {
+            if (arr.is_array()) {
+                for (auto& phrase : arr) {
+                    dialogues[key].push_back(phrase.get<std::string>());
+                }
+            }
+        }
+    }
+}
+
+std::string Creature::GetName() const {
+    return name;
+}
+
+int Creature::GetStrength() const {
+    return strength;
+}
+
 int Creature::GetHp() const
 {
     return hp;
@@ -13,6 +72,10 @@ int Creature::GetArmor() const
 int Creature::GetMoney() const
 {
     return money;
+}
+
+std::unordered_map <std::string, std::vector<std::string>> Creature::GetDialogues() {
+    return dialogues;
 }
 
 std::vector<std::shared_ptr<Item>>& Creature::GetInventory()
--- CGameProject/CGameProject/Creature.h
+++ CGameProject/CGameProject/Creature.h
@@ -3,18 +3,36 @@
 
 #include <vector>
 #include <memory>
-
+#include <string>
+#include <unordered_map>
 #include "Item.h"
+#include "json.hpp"
+#include <fstream>
+#include <iostream>
+
+
+using json = nlohmann::json;
 
 class Creature
 {
 private:
-	std::vector<std::shared_ptr<Item>> inventory;
+	std::string name;
 	int hp;
 	int armor;
 	int money;
+	int strength;
+
+	std::unordered_map <std::string, std::vector<std::string>> dialogues;
+	std::vector <std::shared_ptr<Item>> inventory;
+
 public:
+	Creature(const std::string& path);
+
+	std::unordered_map <std::string, std::vector<std::string>> GetDialogues();
 	std::vector<std::shared_ptr<Item>>& GetInventory();
+
+	std::string GetName() const;
+	int GetStrength() const;
 	int GetHp() const;
 	int GetArmor() const;
 	int GetMoney() const;
